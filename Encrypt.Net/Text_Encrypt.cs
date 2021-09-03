aff262ca2a54d34aec3d5c1f872eb65b20872eab
Jose_LeonJL <60480200+Jose-LeonJL@users.noreply.github.com>
2021-09-02 19:03:06 -0600

    Inicializacion de clases

---
+++ Encrypt.Net/Text_Encrypt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Encrypt.Net.Text
+{
+    public class Text_Encrypt
+    {
+        //Variables
+        private string _Contraseña;
+        private byte[] _Contraseña_Bytes;
+        
+        //Propiedades
+
+        //Constructores
+        public Text_Encrypt(string Key)
+        {
+            this._Contraseña = Key;
+            this._Contraseña_Bytes = Encoding.UTF8.GetBytes(Key);
+        }
+        public Text_Encrypt(Byte[] Key)
+        {
+            this._Contraseña = Encoding.UTF8.GetString(Key);
+            this._Contraseña_Bytes = Key;
+        }
+
+    }
+}
