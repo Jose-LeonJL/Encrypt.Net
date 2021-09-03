4773ad51496c2e5dfa57a582dc84b31a28bdef8e
Jose_LeonJL <60480200+Jose-LeonJL@users.noreply.github.com>
2021-09-02 21:36:26 -0600

    Creacion de las primeras funciones de cifrado

--- Encrypt.Net/Text_Encrypt.cs
+++ Encrypt.Net/Text_Encrypt.cs
@@ -9,8 +9,15 @@ namespace Encrypt.Net.Text
         //Variables
         private string _Contraseña;
         private byte[] _Contraseña_Bytes;
-        
+
         //Propiedades
+        public string Key
+        {
+            get
+            {
+                return this._Contraseña;
+            }
+        }
 
         //Constructores
         public Text_Encrypt(string Key)
@@ -18,11 +25,47 @@ namespace Encrypt.Net.Text
             this._Contraseña = Key;
             this._Contraseña_Bytes = Encoding.UTF8.GetBytes(Key);
         }
+
         public Text_Encrypt(Byte[] Key)
         {
             this._Contraseña = Encoding.UTF8.GetString(Key);
             this._Contraseña_Bytes = Key;
         }
 
+        //Procesos y funciones
+        public string sha256(string Text)
+        {
+            string Resultado;
+            try
+            {
+                using var sha256 = SHA256.Create();
+                byte[] Body = sha256.ComputeHash(Encoding.UTF8.GetBytes(Text));
+                var Constructor = new StringBuilder();
+                for (int i = 0; i < Body.Length; i++)
+                {
+                    Constructor.Append(Body[i].ToString("x2"));
+                }
+                Resultado = Constructor.ToString();
+            }
+            catch( Exception ex)
+            {
+                throw ex;
+            }
+            return Resultado;
+        }
+
+        public string base64(string text)
+        {
+            string Resultado;
+            try
+            {
+                byte[] Body = Encoding.UTF8.GetBytes(text);
+                Resultado = Convert.ToBase64String(Body);
+            }catch (Exception ex)
+            {
+                throw ex;
+            }
+            return Resultado;
+        }
     }
 }
---
+++ Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Encrypt.Net.Text;
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Text_Encrypt _Encrypt;
+            _Encrypt = new Text_Encrypt("Hola");
+            Console.WriteLine($"Cifrado SHA256 {_Encrypt.sha256("Hello World!")} \nCifrado base64 {_Encrypt.base64(_Encrypt.sha256("Hello World!"))}");
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
